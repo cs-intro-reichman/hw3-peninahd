fb96b9daca04ab5c40c8459697c77c040822e913
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-21 20:28:19 +0200

    submitting Anagram

--- Anagram.java
+++ Anagram.java
@@ -1,7 +1,10 @@
+import java.util.regex.Pattern;
+
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
+		randomAnagram("hello");
 		System.out.println(isAnagram("silent","listen"));  // true
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
@@ -28,22 +31,61 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		//wanna get rid of the spaces to not mess with the length of the strings
+		str1 = str1.replace(" ", "");
+		str2 = str2.replace(" ", "");
+
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		
+		for (int i = 0; i < str1.length(); i++) {
+				char c = str1.charAt(i);
+				if (str2.indexOf(c) == -1) {
+					return false;
+				} else {
+					str2 = str2.replaceFirst(String.valueOf(c), "");
+				}
+			}
+			return true;
+		}
+	
+	//this seems very compact and efficient i like it
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		int i = 0;
+		String forbiddenCharacters = "0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+		StringBuilder result = new StringBuilder();
+
+		while (i < str.length()) {
+			char c = str.charAt(i);
+			if (Character.isUpperCase(c)) {
+				c = Character.toLowerCase(c);
+			}
+			
+			if (forbiddenCharacters.indexOf(c) == -1) {
+				result.append(c);
+			}
+
+			i++;
+		}
+
+		return result.toString();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = preProcess(str);
+		str = str.replace(" ", "");
+		StringBuilder result  = new StringBuilder();
+		while (str.length() > 0){
+			int i = (int) (Math.random() * str.length());
+			result.append(str.charAt(i));
+			str = str.substring(0, i) + str.substring(i + 1);
+		}
+
+		return result.toString();
 	}
 }
