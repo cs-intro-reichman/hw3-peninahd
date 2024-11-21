2d10a75f28e713955ab6a7169813b64d6dd6740c
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-21 18:31:18 +0200

    deleted an accidental extra println

--- LoanCalc.java
+++ LoanCalc.java
@@ -23,7 +23,6 @@ public class LoanCalc {
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
-		System.out.println(endBalance(100000, 5, 10, 10000));
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
